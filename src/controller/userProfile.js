8e053a6aa19a488c9efb26bfa59c28dff48eaaac
AlRafi19 <aalzaman19@gmail.com>
2023-11-18 14:15:07 +0600

    done user Profile

--- src/controller/userProfile.js
+++ src/controller/userProfile.js
@@ -1,2 +1,18 @@
-const mac = "pera"
-const mac2 = "pera nai xill"
+const {UserProfileSave,UserProfileDetails} = require("../services/userServices")
+
+exports.CreateProfile=async(req,res)=>{
+    let result = await UserProfileSave(req)
+    return res.status(200).json(result)
+}
+exports.ReadProfile=async(req,res)=>{
+
+    let result = await UserProfileDetails(req)
+    return res.status(200).json(result)
+}
+
+exports.UpdateProfile=async(req,res)=>{
+
+    let result = await UserProfileSave(req)
+    return res.status(200).json(result)
+
+}
--- src/services/userServices/userServices.js
+++ src/services/userServices/userServices.js
@@ -54,3 +54,35 @@ exports.userVerify = async (req, res) => {
     return { error: "Internal Server Error" };
   }
 };
+
+// User Profile (Create,Read,Update)
+
+// Profile Create & Update 
+
+const UserProfileSave = async (req) => {
+  try {
+      let user_id = req.headers.id;
+      let reqBody = req.body;
+      reqBody.userID = user_id;
+      await ProfileModel.updateOne({ userID: user_id }, { $set: reqBody }, { upsert: true });
+      return { status: "success", message: "Profile Save Changed" };
+  } catch (e) {
+      console.error("Error in updating profile:", e); // Log the error for debugging
+      return { status: "fail", message: "Something Went Wrong" };
+  }
+};
+
+// User Profile Read
+
+const UserProfileDetails = async (req)=>{
+  try{
+      let user_id=req.headers.id;
+      let data=await ProfileModel.find({userID: user_id})
+      return {status:"success", data:data}
+  }
+  catch (e) {
+      return {status:"fail", data:"Something Went Wrong"}
+  }
+}
+
+module.exports = {UserProfileSave, UserProfileDetails }
